b1583a234c552113d547d37dddf57d71cd748733
domshi <90562421+domshi@users.noreply.github.com>
2025-01-19 18:32:23 +0000

    Initial

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+  public static void hello() {
+    System.out.println("Dominic shriver");
+    System.out.println("Game development");
+    System.out.println("year 2 of college");
+    System.out.println("I am learning these things so that I can make products that will make the world a slighly better place");
+    System.out.println("Something interesting about me would be that I am a paid discord moderator");
+  }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+/*
+Dominic shriver
+Java 2805
+D0 assignment
+12 Jan 2025
+-main class for assigning the hello class
+-the hello class for standardizing the introduction printing
+I DID NOT USE AI TO MAKE THIS PROGRAM
+*/  
+  public class Main { //main class
+  public static void main(String[] args) {//main method
+    //starts by registering the other java file
+    Hello Hello = new Hello();
+    //this starts the hello function inside the Hello class
+    Hello.hello();
+    
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
